57927d2e195ffd62f96aa903223f87410f2b7236
lingphil <phil.ling33@myhunter.cuny.edu>
2016-02-16 14:42:29 -0500

    loginView and registerView
    needs work

--- nylit/nylit/ViewController.swift
+++ nylit/nylit/ViewController.swift
@@ -10,14 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func loginButton(sender: UIButton) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func registerButton(sender: UIButton) {
     }
 
 
